665f309ad088ea2b23e5af5371db8aba1778b617
jt <1465704128@qq.com>
2026-08-14 17:33:37 +0800

    作业和面向对象部分练习

---
+++ day10/day10/Library.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace day10
+{
+    internal class Library
+    {
+        public string path;
+        public List<Dictionary<string, dynamic>> bookLists = new ();
+        public void add(Dictionary<string, dynamic> bookList){
+            Random r = new Random();
+            bookList["id"] = r.NextDouble();
+            bookLists.Add(bookList);
+            File.AppendAllText(path, JsonSerializer.Serialize(bookList));
+        }
+        public void upDate(Dictionary<string, dynamic> newbookList){
+            for (int i = 0; i < bookLists.Count; i++) {
+                if (bookLists[i]["id"] == newbookList["id"] || bookLists[i]["name"] == newbookList["name"])
+                {
+                    foreach (dynamic item in bookLists[i])
+                    {
+                        item[item.key] = newbookList[item.key];
+                    }
+                }
+            }
+            File.WriteAllText(path, JsonSerializer.Serialize(bookLists));
+        }
+        public void delete(string str)
+        {
+            foreach (var item in bookLists) {
+                if (item["id"] == str)bookLists.Remove(item);
+                else if(item["name"] == str)bookLists.Remove(item);
+            }
+            File.WriteAllText(path,JsonSerializer.Serialize(bookLists));
+        }
+        public void queryName()
+        {
+            Console.WriteLine("请输入你想要查询名字");
+            string name = Console.ReadLine();
+            foreach (var item in bookLists) {
+                if (item["name"] == name)
+                {
+                    foreach(var item2 in item)
+                    {
+                        Console.WriteLine(item2);
+                    }
+                }
+            }
+        }
+        public void queryId()
+        {
+            Console.WriteLine("请输入你想要查询的id");
+            double id = double.Parse(Console.ReadLine());
+            foreach (var item in bookLists)
+            {
+                if (item["id"] == id)
+                {
+                    foreach(var item2 in item) Console.WriteLine(item2);
+                }
+            }
+        }
+        public void book(string str)
+        {
+            path = str;
+            Random r = new Random();
+             bookLists = new List<Dictionary<string, dynamic>>() {
+                 
+                new Dictionary<string, dynamic>()
+                {
+                ["name"] = "三国演义",
+                ["author"] = "罗贯中",
+                ["isBorrow"] = true, // false表示还在书库中，true表示外借
+                ["id"] = r.NextDouble(),
+                ["mark"] = "言情、武侠",
+                ["price"] = 56.09 // 价格
+                }
+            };
+            string str1 = JsonSerializer.Serialize(bookLists);
+            if (!File.Exists(str1)) File.WriteAllText(path, str1);
+
+        }
+    }
+}
---
+++ day10/day10/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace day10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Action wrireLog = () => {
+            //    string str = Console.ReadLine();
+            //    var time = DateTime.Now;
+            //    File.AppendAllText(@"./content.log",$"{str+time}\n");
+            //};
+            ////wrireLog();
+            //Func<string, int> isFile0rDic = path => {
+            //    if (File.Exists(path)) return 1;
+            //    if(Directory.Exists(path)) return 2;
+            //    return 0;
+            //};
+            //Func<string, List<string>> fileAndDic = path =>
+            //{
+            //    if (isFile0rDic(path) != 2) throw new Exception("路径异常错误");
+            //    string[] files = Directory.GetFiles(path);
+            //    List<string> list = new List<string>();
+            //    list.AddRange(files);
+            //    string[] dics = Directory.GetDirectories(path);
+            //    list.AddRange(dics);
+            //    return list;
+            //};
+            //List<string> list = fileAndDic("./");
+            //foreach (string file in list) Console.WriteLine(file);
+            //Library library1 = new Library();
+            //library1.book("./books.txt");
+            //library1.queryName();
+            void gitIn()
+            {
+                Console.WriteLine("请输入用户名和密码：");
+                string userName = Console.ReadLine();
+                string password = Console.ReadLine();
+                File.AppendAllText("./user.txt", $"userName= {userName} password= {password}\n");
+                File.AppendAllText("./user.txt", "===\n");
+            }
+        gitIn();
+    }
+    }
+}
